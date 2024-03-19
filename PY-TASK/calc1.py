3bac41ccb75b96de1980fb48708a113581cdda1b
Hariharan2223 <113585610+Hariharan2223@users.noreply.github.com>
2024-03-19 18:23:59 +0530

    Add files via upload

---
+++ PY-TASK/calc1.py
@@ -0,0 +1,27 @@
+def add(a,b):
+    """Addition function"""
+    return a+b
+
+def sub(a,b):
+    """Subtraction  function"""
+    c=a-b
+    if(c>=0):
+        return c
+    else:
+        return -c
+     
+
+def mul(a,b):
+    """Multiplication function"""
+    return a*b
+
+def div(a,b):
+    """division function"""
+    return a/b
+def percentage(a,b):
+    """percentage function"""
+    return b*(a/100)
+
+
+
+
---
+++ day2/modules1.py
@@ -0,0 +1,10 @@
+name="buvi"
+age="23"
+def hi(a,b):
+    return a,b
+
+def hello():
+    """hello buddies1"""
+    return "hi"
+print(name)
+hello()
