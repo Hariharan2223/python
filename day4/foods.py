000ff24d5cf33178634e794cce345b4eefd06434
Hariharan2223 <113585610+Hariharan2223@users.noreply.github.com>
2024-03-21 18:15:42 +0530

    Add files via upload

---
+++ day4/foods.py
@@ -0,0 +1,27 @@
+hotel="Hotel Sri Annamalaiyar"
+
+def idly():
+    item="Idly"
+    rate=10
+    return item,rate
+
+def dosa():
+    item="dosa"
+    rate=50
+    return item,rate
+   
+def poori():
+    item="poori"
+    rate=60
+    return item,rate
+
+def pongal():
+    item="pongal"
+    rate=40
+    return item,rate
+
+def vada():
+    item="vada"
+    rate=15
+    return item,rate
+
